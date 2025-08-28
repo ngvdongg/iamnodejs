24a72b85f60163b518244e2d39114ca07e16e81e
DONGNV\MyPC <dongnv>
2025-08-28 23:03:17 +0700

    init: new project

---
+++ app.js
@@ -0,0 +1,16 @@
+    // app.js
+    const express = require('express');
+    const app = express();
+    const port = 3000;
+    const authRouter = require('./src/auth/auth.routes');
+    const userRouter = require('./src/users/users.routes');
+    app.get('/', (req, res) => {
+      res.send('Hello from Node.js!');
+    });
+    
+    app.use('/auth', authRouter);
+    app.use('/users', userRouter);
+
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    });
